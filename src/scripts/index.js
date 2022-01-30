b86ba1c5fbb3fa80d5bf3d6e889bcad270448abe
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-01-30 12:53:28 -0300

    Put simbols on blocks and add a table score

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,51 @@
 const TABLE_CONTAINER = document.querySelector("#game");
 
 const game = {
+    boardInGame: [],
+    playerSimbol: "X",
+    finish: false,
+    init () {
+        this.draw();
+        this.addEventOnButtons(".block");
+
+    },
+    addEventOnButtons (selector="") {
+        document.querySelectorAll(selector).forEach(el => el.addEventListener("click", (ev) => {
+            this.markBlock(this.playerSimbol, ev.target);
+
+        }));
+
+    },
+    changePlayerSimbol () {
+        console.log("gg")
+        this.playerSimbol = this.playerSimbol === "X" ? "O" : "X";
+
+    },
+    markBlock: (simbol, el) => {
+        if ($(el).hasClass("block") && !$(el).hasClass("marked")) {
+            var pSimbol = document.createElement("p");
+            pSimbol.textContent = simbol;
+
+            el.appendChild(pSimbol);
+            $(el).addClass("marked");
+
+            game.changePlayerSimbol();
+
+        }
+
+    },
     draw: () => {
         const rowEl = [[], [], []];
+        var auxId = 0;
         rowEl.forEach((el, index) => {
             for (let i = 0; i < 3; i++) {
                 var button = document.createElement("div");
                 button.classList.add("block");
+                button.id = auxId;
 
                 el.push(button);
+
+                auxId++;
                 
             }
 
@@ -28,4 +65,4 @@ const game = {
 
 }
 
-game.draw();
+game.init();
